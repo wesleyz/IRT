c7546db24c79cdcd94ca531afcb0a341f9731fa8
syumeikoukyo <kankankan0818@hotmail.com>
2020-05-09 15:06:51 +0900

    コメントの追加

--- irt.py
+++ irt.py
@@ -9,6 +9,16 @@ import utils
 class IRT4Order:
     def __init__(self, df: pd.core.frame.DataFrame,
                  nc: int, stm=None, dat=None, D=1):
+        """
+        Attruute:
+            df: 分析対象のデータフレーム
+            nc: 設問のカテゴリ数 (段階数)
+            stm: stanモデル - defaultではNone
+                 Noneの場合は、set_modelで作成
+            dat: stanモデルに食わせるデータ
+                 Noneの場合は、set_modelで作成
+            D: 定数
+        """
         self.df = df
         self.nj = df.shape[1]
         self.ni = df.shape[0]
@@ -22,6 +32,12 @@ class IRT4Order:
         self.theta = None
 
     def set_model(self, sample=None):
+        """ Summary
+        stanモデルとそれに投入するdatデータの作成
+
+        Input
+            sample: sample数 (Noneの場合はdf内の全てのデータを使う)
+        """
         self.stm = pystan.StanModel(model_code=utils.model)
         if sample is not None:
             self.ni = sample
@@ -32,18 +48,36 @@ class IRT4Order:
 
     def model_fit(self, n_itr=2000, chains=4, n_warmup=1000, n_jpbs=-1,
                   algorithm="NUTS", verbose=False):
+        """ Summary
+        stanモデルの実行
+
+        n_itr:      default = 2000
+        chains:     default = 4
+        n_warmup:   default = 1000
+        n_jobs:     default = -1
+        algorithm:  default = "NUTS
+        verbose:    default = False
+        """
         self.fit = self.stm.sampling(data=self.dat, iter=n_itr,
                                      chains=chains, n_jobs=-1,
                                      warmup=n_warmup, algorithm="NUTS",
                                      verbose=False)
 
     def extract(self):
+        """ Summmary
+        stanモデルの結果であるfitから、推定されたパラメータを取得
+        """
         self.result = self.fit.extract()
         self.alpha = self.result["a"].mean(axis=0)
         self.beta = self.result["b"].mean(axis=0)
         self.theta = self.result["theta"].mean(axis=0)
 
     def plot(self, sort="Q", save_name="irt_Q"):
+        """ Summary
+        sort: プロットする時、設問ごとにするか段階ごとにするか default = "Q"
+              Q: 設問ごと, L: 段階ごと
+        save_name: 保存する画像名(拡張子は含めない) default = "irt_Q"
+        """
         theta_ = np.arange(np.min(self.theta), np.max(self.theta),
                            step=(np.max(self.theta)
                                  - np.min(self.theta))/self.ni)
